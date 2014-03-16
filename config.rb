3e4ad963053b1bc3ea68a4cbbb1ef3dcf128c0eb
Joseph G <gamboajosephd@gmail.com>
2014-03-16 21:58:25 +0800

    Test commit

--- config.rb
+++ config.rb
@@ -36,7 +36,7 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -53,18 +53,37 @@ set :images_dir, 'img'
 
 # Build-specific configuration
 configure :build do
+
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+activate :blog do |blog|
+  blog.prefix = "posts"
+  blog.permalink = ":year/:month/:day/:title.html"
+  #blog.sources = "/posts/:year-:month-:day-:title.html"
+  blog.tag_template = "tag.html"
+  blog.paginate = true
+end
+
+activate :directory_indexes
+
+set :relative_links, true
+
+activate :syntax
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true
+
+
